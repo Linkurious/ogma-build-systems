2620bb366bb5de29c1c3859ae156a9db10de7e11
w8r <alex@milevski.co>
2024-02-05 14:48:12 +0100

    Updated rollup to v4

--- rollup/rollup.config.ts
+++ rollup/rollup.config.ts
@@ -1,7 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import styles from "rollup-plugin-styles";
 import image from "@rollup/plugin-image";
 
 export default [
@@ -9,7 +8,7 @@ export default [
     input: "src/index.ts",
     output: {
       file: "public/bundle.js",
-      format: "umd",
+      format: "iife",
       sourcemap: true,
     },
     // order of plugins is important
