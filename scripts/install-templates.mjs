7d511f6710e65e6c5579cc89bc4f23dae2aa3433
w8r <alex@milevski.co>
2026-04-15 17:46:40 +0200

    Try different order

--- scripts/install-templates.mjs
+++ scripts/install-templates.mjs
@@ -1,8 +1,10 @@
 import { execSync } from 'node:child_process';
 import { root, templates } from './utils.mjs';
 
+const userconfig = root('.npmrc');
+
 for (const t of templates) {
   const flags = t === 'parcel' ? '--ignore-scripts' : '';
   console.log(`Installing ${t}...`);
-  execSync(`npm i ${flags}`, { cwd: root('templates', t), stdio: 'inherit' });
+  execSync(`npm i ${flags} --userconfig "${userconfig}"`, { cwd: root('templates', t), stdio: 'inherit' });
 }
