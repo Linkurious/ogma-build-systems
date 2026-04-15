b268d198652aebf0e60b64a19de8fb6cf769f472
w8r <alex@milevski.co>
2026-04-15 17:35:44 +0200

    Try different order

--- scripts/install-templates.mjs
+++ scripts/install-templates.mjs
@@ -1,13 +1,6 @@
 import { execSync } from 'node:child_process';
 import { root, templates } from './utils.mjs';
 
-// During npm pack, templates don't need their deps installed
-const npmCommand = process.env.npm_command;
-if (npmCommand === 'pack') {
-  console.log('Skipping template installs (npm pack)');
-  process.exit(0);
-}
-
 for (const t of templates) {
   const flags = t === 'parcel' ? '--ignore-scripts' : '';
   console.log(`Installing ${t}...`);
